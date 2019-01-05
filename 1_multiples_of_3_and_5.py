433677cc9ef10f01d7a251a2f1b32e2260ba0663
Esther Khew <paulinakhew12345@hotmail.com>
2019-01-05 01:14:45 -0500

    :recycle: Factor the equations used to find the multiples of 3 and 5

--- 1_multiples_of_3_and_5.py
+++ 1_multiples_of_3_and_5.py
@@ -17,7 +17,7 @@ for a0 in range(t): #iterates through each inputted number
 
     #we find the sum of all the multiples of 3 and 5 using the appropriate formula
     #subtract all the multiples of 15 as they are counted twice
-    sum = 3 * ( a * (a + 1)) // 2 + 5 * ( b * (b + 1)) // 2 - 15 * ( c * (c + 1)) // 2
-
+    sum = (3 * ( a * (a + 1)) + 5 * ( b * (b + 1)) - 15 * ( c * (c + 1))) // 2
+    
     #print the sum
     print(sum)
