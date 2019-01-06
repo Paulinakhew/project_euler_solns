d50e0919da0ea4290c90f15043cac00f0fccea40
Esther Khew <paulinakhew12345@hotmail.com>
2019-01-05 20:21:34 -0500

    :zap: Improve performance of code to pass all test cases
    Received perfect score

--- 6_sum_square_difference.py
+++ 6_sum_square_difference.py
@@ -8,6 +8,6 @@ for a0 in range(t):
     n = int(input().strip())
     sum_num = sum([x for x in range(n+1)])
     square_sum = sum_num ** 2
-    sum_square = sum([x ** 2 for x in range(n+1)])
+    sum_square = (n * (n + 1) * (2 * n + 1)) // 6
     abs_diff = square_sum - sum_square
     print(abs_diff)
