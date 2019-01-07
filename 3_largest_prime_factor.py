204d27d426cb6e3a4ebd6b11afb85899bfc40ab5
Esther Khew <paulinakhew12345@hotmail.com>
2019-01-06 19:40:57 -0500

    :zap: Write code to get perfect score

--- 3_largest_prime_factor.py
+++ 3_largest_prime_factor.py
@@ -1,17 +1,21 @@
 #!/bin/python3
 
 import sys
+import math
 
 t = int(input().strip())
 for a0 in range(t):
     n = int(input().strip())
-    d = 2
-    """while n > 1:
-        while n % d == 0:
-            highest_factor = d
-            n = n / d    
-        d = d + 1"""
-    while n % d == 2:
-        n /= 2
+    highest_factor = 1
+    while n % 2 == 0:
+        highest_factor = 2
+        n //= 2
+    #at this point the number is odd
+    for i in range(3, int(math.sqrt(n)) + 1, 2): 
+        while n % i == 0: 
+            highest_factor = i 
+            n = n // i 
+    if n > 2: 
+        highest_factor = n 
     
     print(highest_factor)
